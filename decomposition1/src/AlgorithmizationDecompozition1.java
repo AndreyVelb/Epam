44809c997ae874f1541e6be4062a15e94c0b6efc
Jangy1996 <77194928+Jangy1996@users.noreply.github.com>
2021-04-18 13:22:35 +0300

    2_Algorithmization

---
+++ decomposition1/src/AlgorithmizationDecompozition1.java
@@ -0,0 +1,25 @@
+
+// Написать метод(методы) для нахождения наибольшего общего делителя и наименьшего общего кратного двух
+//натуральных чисел: НОК(А,В) = А*В/НОД(А,В);
+
+public class AlgorithmizationDecompozition1 {
+    public static void main(String[] args) {
+        int a = 36;
+        int b = 24;
+        System.out.println(greatestCommonFactor(a,b));
+        System.out.println(leastCommonMultiple(a,b));
+    }
+    public static int greatestCommonFactor(int a, int b){
+        int max = a > b ? a : b;
+        for(int i = max; i > 1; i--){
+            if(a % i == 0 && b % i == 0){
+                return i;
+            }
+        }
+        return 1;
+    }
+    public static int leastCommonMultiple(int a, int b){
+        int lcm = a*b/greatestCommonFactor(a,b);
+        return lcm;
+    }
+}
